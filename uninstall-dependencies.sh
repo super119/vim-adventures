5fa1803034857feb84070b28d78f77fd871a8b84
Mark Zhang <ace119@163.com>
2022-01-16 14:53:56 +0800

    Adding uninstall-dependencies.sh
    Signed-off-by: Mark Zhang <ace119@163.com>

---
+++ uninstall-dependencies.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo
+echo "This script is supposed to run individually."
+echo "I.E: please do NOT run it in 'vim-adventures' repository folder."
+echo
+echo "This script will remove the entire $HOME/.vim folder."
+read -p "Go? (Y/n): " ANS
+if [ "$ANS" != "" ] && [ "$ANS" != "y" ] && [ "$ANS" != "Y" ] ; then
+	exit 0
+fi
+
+echo
+echo "Uninstalling gtags..."
+cd ~
+wget -c "https://ftp.gnu.org/pub/gnu/global/global-6.6.8.tar.gz"
+tar zxvf global-6.6.8.tar.gz
+cd global-6.6.8
+./configure --prefix=$HOME/.local
+make uninstall
+cd -
+
+echo
+echo "Uninstalling vim..."
+cd ~
+git clone https://github.com/vim/vim.git
+git checkout -b v8.2.4103 v8.2.4103
+cd vim
+./configure --prefix=$HOME/.local --enable-pythoninterp=yes --enable-python3interp=yes
+make install
+cd -
+
+sed -i "/export PATH=\$HOME\/.local\/bin:\$PATH/d" ~/.bashrc
+sed -i "/alias vi=/d" ~/.bashrc
+
+cd ~
+rm -rf global-6.6.8 global-6.6.8.tar.gz vim
+rm -rf .vim
+cd -
+
+echo "Done."
